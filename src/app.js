a7b0cd9934e4a5d59357f0f5d9d3c1ec4b6a86ec
Wasim Beniwale <wasimb4@gmail.com>
2023-12-03 17:52:41 +0530

    small bug fixes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-function displayQRCode(locationText){
-    var qrcode = new QRCode(document.getElementById("qrcode"), {
+function displayQRCode(locationText) {
+    new QRCode(document.getElementById("qrcode"), {
         text: locationText,
         width: 128,
         height: 128,
-        colorDark : "#000000",
-        colorLight : "#ffffff",
-        correctLevel : QRCode.CorrectLevel.H
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H,
     });
 }
 
 //thanks to http://stackoverflow.com/a/12189958
 chrome.tabs.query(
     {
-        currentWindow: true, 
-        active: true
+        currentWindow: true,
+        active: true,
     },
     function (foundTabs) {
-        if(foundTabs.length > 0){
+        if (foundTabs.length > 0) {
             var url = foundTabs[0].url;
-            displayQRCode(url)
+            displayQRCode(url);
         }
     }
 );
